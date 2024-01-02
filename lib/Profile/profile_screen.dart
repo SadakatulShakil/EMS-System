44e480b39ca9080b2c24721a2357838f29f532cd
kabir808 <kabir.softwindtech@gmail.com>
2024-01-02 15:11:45 +0600

    profile page, home time date and time wise gretings done

--- lib/Profile/profile_screen.dart
+++ lib/Profile/profile_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../../utill/color_resources.dart';
 class ProfileScreen extends StatefulWidget {
   bool backExits;
   ProfileScreen({Key? key, required this.backExits}) : super(key: key);
@@ -15,6 +14,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
     super.initState();
 
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,30 +22,154 @@ class _ProfileScreenState extends State<ProfileScreen> {
           child: Column(
             children: [
               SizedBox(height: 50,),
+              Align(
+                alignment: Alignment.center,
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/user.png',),
+                  radius: 80,
+                ),
+              ),
+              SizedBox(height: 8,),
+              Align(
+                alignment: Alignment.center,
+                child: Text('Mr. Haasan Masud', style: TextStyle(fontSize: 28, fontWeight: FontWeight.w600, color: Colors.grey[700]),)
+              ),
+              SizedBox(height: 8,),
+              Align(
+                  alignment: Alignment.center,
+                  child: Text('Senior Software Engineer', style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600, color: Colors.grey[500]),)
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                child: Divider(thickness: 1,),
+              ),
+              SizedBox(height: 8,),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    Text('Department: '),
+                    Text('Technology')
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Email: '),
+                    Text('haasanmanud@company.com')
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Phone: '),
+                    Text('+8801751330394')
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Blood group: '),
+                    Text('A(ev+)')
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                child: Divider(thickness: 1,),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8),
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
                       children: [
-                        Text('Hey! Haasan Masud', style: TextStyle(fontSize: 25 / MediaQuery.textScaleFactorOf(context)),),
-                        SizedBox(height: 10,),
-                        Text('Welcome to Softwind Tech LTD.', style: TextStyle(fontSize: 18 / MediaQuery.textScaleFactorOf(context)),),
+                        Icon(Icons.settings, size: 25,),
+                        SizedBox(width: 10,),
+                        Text('Settings', style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),)
                       ],
                     ),
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(100.0),
-                      child: Image.asset('assets/images/user.png',height: 70,width: 70,)
-                    )
-                  ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8),
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Icon(Icons.history, size: 25,),
+                        SizedBox(width: 10,),
+                        Text('Pending request', style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),)
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8),
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Icon(Icons.history_edu, size: 25,),
+                        SizedBox(width: 10,),
+                        Text('Leave History', style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),)
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0, right: 8),
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Icon(Icons.history, size: 25,),
+                        SizedBox(width: 10,),
+                        Text('Attendance History', style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),)
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 15,),
+              Container(
+                width: MediaQuery.of(context).size.width*.5,
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Icon(Icons.logout, size: 25, color: Colors.red,),
+                        SizedBox(width: 10,),
+                        Text('Logout', style: TextStyle(fontWeight: FontWeight.w400, fontSize: 20, color: Colors.red),)
+                      ],
+                    ),
+                  ),
                 ),
               ),
             ],
           ),
-        ));
+        )
+    );
   }
 }
 
--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,17 +1,52 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import '../../utill/color_resources.dart';
+import 'package:intl/intl.dart';
+import 'package:stream/stream.dart';
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
+  late String currentTime;
+  late String currentDate;
+  late String greeting;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+// Initial update
+    _updateDateTime();
 
+    // Start a periodic timer to update every minute
+    Timer.periodic(Duration(minutes: 1), (timer) {
+      _updateDateTime();
+    });
+  }
+
+  void _updateDateTime() {
+    final now = DateTime.now();
+
+    // Format time as "09 : 00 AM"
+    currentTime = DateFormat.jm().format(now);
+
+    // Format date as "01/01/2024, Monday"
+    currentDate = DateFormat('MM/dd/yyyy, EEEE').format(now);
+
+    // Determine the greeting based on the time
+    if (now.isBefore(DateTime(now.year, now.month, now.day, 12, 0))) {
+      greeting = 'Good morning, \r\nMark your Attendance time.';
+    } else if (now.isBefore(DateTime(now.year, now.month, now.day, 18, 0))) {
+      greeting = 'Good afternoon, \r\nEnsure lunch and sit again.';
+    } else if (now.isBefore(DateTime(now.year, now.month, now.day, 20, 0))) {
+      greeting = 'Good evening, \r\nMark your sign out time.';
+    } else {
+      greeting = 'Good night, \r\nRelax & have a sweet dream.';
+    }
+
+    setState(() {}); // Update the UI with the new values
   }
   @override
   Widget build(BuildContext context) {
@@ -32,7 +67,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         Text('Hey, Haasan Masud!', style: TextStyle(
                             fontSize: 25 / MediaQuery.textScaleFactorOf(context), fontWeight: FontWeight.w600),),
                         SizedBox(height: 10,),
-                        Text('Good Morning, Mark your Attendance.', style: TextStyle(
+                        Text(greeting, style: TextStyle(
                             fontSize: 15 / MediaQuery.textScaleFactorOf(context), color: Colors.grey[600]),),
                       ],
                     ),
@@ -44,11 +79,121 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
               ),
               SizedBox(height: 30,),
-              Text('09 : 00 AM', style: TextStyle(fontSize: 30 / MediaQuery.textScaleFactorOf(context), ),),
-              Text('01/01/2024, Monday', style: TextStyle(
+              Text(currentTime, style: TextStyle(
+                fontSize: 30 / MediaQuery.textScaleFactorOf(context), ),),
+              Text(currentDate, style: TextStyle(
                   fontSize: 15 / MediaQuery.textScaleFactorOf(context), color: Colors.grey[600]),),
-
-              SizedBox(height: 100,),
+              SizedBox(height:16,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width*.4,
+                      child: Card(
+                          child: Column(
+                        children: [
+                          Container(
+                            width: MediaQuery.of(context).size.width,
+                            color: accent,
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Align(
+                                  alignment: Alignment.center,
+                                    child: Text('Attendance', style: TextStyle(fontSize: 18, color: Colors.white),)),
+                              )),
+                          SizedBox(height: 8,),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 5),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Working day: ',style: TextStyle(color: Colors.blueAccent)),
+                                Text('25',style: TextStyle(color: Colors.blueAccent)),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 5),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('On time: ',style: TextStyle(color: Colors.green)),
+                                Text('20',style: TextStyle(color: Colors.green)),
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 5),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Late entry: ', style: TextStyle(color: Colors.redAccent),),
+                                Text('5', style: TextStyle(color: Colors.redAccent)),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 8,),
+                        ],
+                      )),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width*.4,
+                      child: Card(
+                          child: Column(
+                            children: [
+                              Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  color: accent,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(5.0),
+                                    child: Align(
+                                        alignment: Alignment.center,
+                                        child: Text('Leave', style: TextStyle(fontSize: 18, color: Colors.white),)),
+                                  )),
+                              SizedBox(height: 8,),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 5),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text('Total Leave: ',style: TextStyle(color: Colors.blueAccent)),
+                                    Text('25',style: TextStyle(color: Colors.blueAccent)),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 5),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text('Leave Use: ',style: TextStyle(color: Colors.green)),
+                                    Text('12',style: TextStyle(color: Colors.green)),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 5),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text('Remaining: ', style: TextStyle(color: Colors.redAccent),),
+                                    Text('13', style: TextStyle(color: Colors.redAccent)),
+                                  ],
+                                ),
+                              ),
+                              SizedBox(height: 8,),
+                            ],
+                          )),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20,),
               Container(
                   height: 170, width:170,
                   decoration: BoxDecoration(
@@ -77,7 +222,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     )
                   ] )
               ),
-              SizedBox(height: 50,),
+              SizedBox(height: 20,),
               Padding(
                 padding: const EdgeInsets.all(15.0),
                 child: Row(
