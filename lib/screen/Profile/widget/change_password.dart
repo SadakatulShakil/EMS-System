d957d5a16afbce9e52390296322a61818fa8a60b
SadakatulShakil <sadakatulshakil94@gmail.com>
2024-04-21 12:58:12 +0600

    Login session infinity, Attendance remarks logic update, Profile photo clickable to update, Forget Password, Change the date pattern to 21 April, 2024

--- lib/screen/Profile/widget/change_password.dart
+++ lib/screen/Profile/widget/change_password.dart
@@ -104,7 +104,11 @@ class ChangePasswordScreenState extends State<ChangePasswordScreen> {
       }
       rethrow;
     }
-  }
+  }2. Login session infinity
+  3. Attendance remarks logic update
+  4. Profile photo clickable to update
+  5. Forget Password
+  6. Change the date pattern to 21 April, 2024
 
   @override
   Widget build(BuildContext context) {
